feb2b6ef7b8210ade5c8e6a1e1da42997b753493
Hyjale <saishidual@gmail.com>
2020-09-05 02:04:03 -0500

    changed font to source pro and adjusted line spacing

--- init.el
+++ init.el
@@ -46,5 +46,12 @@
 (require 'org)
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 115 :width normal :family "Source Code Pro")))))
+
 (provide 'init)
 ;;; init.el ends here
